a1b7c496e31bce68cb42ecff16954352dc35f0cd
Андрей Скляр <andreas.skliar@gmail.com>
2023-11-23 21:03:59 +0200

    без локалсториджа

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { App } from 'components/App';
 import './index.css';
-import { persistor, store } from 'redux/store';
+import { store } from 'redux/store';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/goit-react-hw-07-phonebook">
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </BrowserRouter>
 );
